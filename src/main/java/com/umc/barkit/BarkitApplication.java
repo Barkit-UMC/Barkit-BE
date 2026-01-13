ec18dbe08298371615b8794e96ac1773faa8383e
zhwan2845 <johaeun0802@naver.com>
2026-01-13 09:35:46 +0900

    [Chore]:초기세팅

---
+++ src/main/java/com/umc/barkit/BarkitApplication.java
@@ -0,0 +1,13 @@
+package com.umc.barkit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarkitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarkitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/umc/barkit/BarkitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umc.barkit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarkitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
