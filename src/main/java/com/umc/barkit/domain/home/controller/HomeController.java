07ac179caee00417026016b178f9c5929a004ef3
kooinkyu <rndlsrb7@gmail.com>
2026-01-30 18:55:52 +0900

    docs: 홈 대시보드 및 즐겨찾기 API Swagger 문서 추가

--- src/main/java/com/umc/barkit/domain/home/controller/HomeController.java
+++ src/main/java/com/umc/barkit/domain/home/controller/HomeController.java
@@ -4,6 +4,7 @@ import com.umc.barkit.domain.home.dto.response.HomeDashboardResponse;
 import com.umc.barkit.domain.home.service.HomeDashboardService;
 import com.umc.barkit.global.apiPayload.ApiResponse;
 import com.umc.barkit.global.apiPayload.code.GeneralSuccessCode;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +18,11 @@ public class HomeController {
 
     private final HomeDashboardService homeDashboardService;
 
+    @Operation(
+            summary = "홈 대시보드 조회",
+            description = "홈 화면 진입 시 필요한 핵심 정보를 한 번에 조회합니다. " +
+                    "즐겨찾기 매장 요약, 보유 멤버십 카드 정보를 포함합니다."
+    )
     @GetMapping("/dashboard")
     public ResponseEntity<ApiResponse<HomeDashboardResponse.DashboardDTO>> getHomeDashboard() {
 
