ed4e6b67fd32d631f266d0d86a2c979a4201a0db
Seoyeon-park0103 <seoyeon-park@naver.com>
2026-02-02 16:56:41 +0900

    [Feat] 멤버십 바코드 조회 기능 API (매장 상세 페이지)

---
+++ src/main/java/com/umc/barkit/domain/membership/exception/code/MembershipSuccessCode.java
@@ -0,0 +1,4 @@
+package com.umc.barkit.domain.membership.exception.code;
+
+public enum MembershipSuccessCode {
+}
--- src/main/java/com/umc/barkit/domain/membership/repository/UserMembershipBrandRepository.java
+++ src/main/java/com/umc/barkit/domain/membership/repository/UserMembershipBrandRepository.java
@@ -4,7 +4,11 @@ import com.umc.barkit.domain.membership.entity.UserMembershipBrand;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserMembershipBrandRepository extends JpaRepository<UserMembershipBrand, Long>, UserMembershipBrandRepositoryCustom {
+
+    Optional<UserMembershipBrand> findByUserIdAndMembershipBrandId(Long userId,Long membershipBrandId);
 
 }
