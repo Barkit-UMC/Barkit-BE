1f23c82ff7b2cd27221d076752deaf14849e0c81
윤예림 <yereme@yun-yelim-ui-MacBookPro.local>
2026-02-09 15:52:54 +0900

    [feat] 지도 검색 api 성능 개선을 위해 redis 적용

--- src/main/java/com/umc/barkit/domain/store/RedisSmokeTest.java
+++ src/main/java/com/umc/barkit/domain/store/RedisSmokeTest.java
@@ -3,11 +3,13 @@ package com.umc.barkit.domain.store;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
+@Profile({"local", "prod"})
 @Slf4j
 public class RedisSmokeTest implements CommandLineRunner {
 
