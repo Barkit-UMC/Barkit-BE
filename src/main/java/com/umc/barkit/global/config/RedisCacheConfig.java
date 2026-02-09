979b64a473a05181513d554f12eb7b9a52dc07cd
윤예림 <yereme@yun-yelim-ui-MacBookPro.local>
2026-02-09 15:30:13 +0900

    application profile 분리

--- src/main/java/com/umc/barkit/global/config/RedisCacheConfig.java
+++ src/main/java/com/umc/barkit/global/config/RedisCacheConfig.java
@@ -1,5 +1,6 @@
 package com.umc.barkit.global.config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,6 +16,7 @@ import java.util.Map;
 
 @Configuration
 @EnableCaching
+@ConditionalOnProperty(name = "spring.data.redis.enabled", havingValue = "true")
 public class RedisCacheConfig {
 
     @Bean
